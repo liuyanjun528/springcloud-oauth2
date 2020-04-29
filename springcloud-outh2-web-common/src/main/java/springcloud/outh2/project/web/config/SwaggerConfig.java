1296033d315c9415ccf95d5846838e8ff40f119d
yanjun.liu <953038659@qq.com>
2020-04-29 10:13:08 +0800

    添加swagger 请求头

--- springcloud-outh2-web-common/src/main/java/springcloud/outh2/project/web/config/SwaggerConfig.java
+++ springcloud-outh2-web-common/src/main/java/springcloud/outh2/project/web/config/SwaggerConfig.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
@@ -31,7 +37,8 @@ public class SwaggerConfig {
                     .select()// 构建api选择器     会扫描注解了又controller的类
                     .apis(RequestHandlerSelectors.basePackage("springcloud.outh2.project"))// api选择器选择api的包
                     .paths(PathSelectors.any())// api选择器选择包路径下任何api显示在文档中
-                    .build();// 创建文档
+                    .build()
+                    .globalOperationParameters(getPrameterList());// 设置api 请求头
         }
 
         private ApiInfo apiInfo() {
@@ -44,6 +51,16 @@ public class SwaggerConfig {
                     //描述
                     .description("API 接口详情描述")
                     .build();
+        }
+
+        private List<Parameter> getPrameterList() {
+            ParameterBuilder ticketPar = new ParameterBuilder();
+            List<Parameter> pars = new ArrayList<>();
+            ticketPar.name("Authorization").description("oAuth2 JWT ")
+                    .modelRef(new ModelRef("string")).parameterType("header")
+                    .required(false).build(); //header中的ticket参数非必填，传空也可以
+            pars.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+            return pars;
         }
 
     }
